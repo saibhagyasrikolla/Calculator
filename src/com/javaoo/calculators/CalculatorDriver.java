ad57c8fddfbaa40e8884ef62548e420e1ee56469
Saibhagyasri Kolla <134484859+saibhagyasrikolla@users.noreply.github.com>
2024-03-24 16:02:26 +0530

    Update CalculatorDriver.java

--- src/com/javaoo/calculators/CalculatorDriver.java
+++ src/com/javaoo/calculators/CalculatorDriver.java
@@ -4,20 +4,19 @@ public class CalculatorDriver {
 
 	public static void main(String[] args) {
 		
-		BasicCalculator bc =  new BasicCalculator();
-		ScientificCalculator sc = new ScientificCalculator();
-		TrignometricCalculator tc = new TrignometricCalculator();
-		
+		BasicCalculator bc =  new BasicCalculator();	
 		System.out.println(bc.add(5, 4));
 		System.out.println(bc.subtract(5, 4));
 		System.out.println(bc.multiply(5, 4));
 		System.out.println(bc.divide(5, 4));
-		
+
+		ScientificCalculator sc = new ScientificCalculator();
 		System.out.println(sc.exp(7));
 		System.out.println(sc.log(7));
 		sc.putValueInMemory(7);
 		System.out.println(sc.getValueFromMemory());
-		
+
+		TrignometricCalculator tc = new TrignometricCalculator();
 		System.out.println(tc.sine(90));
 		System.out.println(tc.cosine(90));
 		System.out.println(tc.tangent(90));
